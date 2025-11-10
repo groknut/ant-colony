c3776336a9151989b850cc26daa978d9914e6e92
groknut <184639205+groknut@users.noreply.github.com>
2025-11-10 16:33:22 +0800

    add args in .bat files

--- run.bat
+++ run.bat
@@ -11,5 +11,10 @@ type .\input\1.txt
 
 .\aco.exe config.ic
 
-uv run ./main.py ./output/output.csv
-
+if "%~1" == "" (
+	echo No args provided
+	uv run ./main.py ./output/output.csv
+) else (
+	echo Argument provided: %~1
+	uv run ./main.py "%~1" ./output/output.csv
+)
