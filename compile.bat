82aed79542bf892325463c08bbe4ee41a838cd93
groknut <184639205+groknut@users.noreply.github.com>
2025-11-17 14:58:29 +0800

    update .bat files for compile and run porjects

--- compile.bat
+++ compile.bat
@@ -16,10 +16,4 @@ if %errorlevel% neq 0 (
 
 .\aco.exe config.ic
 
-if "%~1" == "" (
-	echo No args provided
-	uv run ./main.py ./output/output.csv
-) else (
-	echo Argument provided: %~1
-	uv run ./main.py "%~1" ./output/output.csv
-)
+uv run ./main.py ./output/output.csv
--- run.bat
+++ run.bat
@@ -8,10 +8,4 @@ echo run project...
 
 .\aco.exe config.ic
 
-if "%~1" == "" (
-	echo No args provided
-	uv run ./main.py ./output/output.csv
-) else (
-	echo Argument provided: %~1
-	uv run ./main.py "%~1" ./output/output.csv
-)
+uv run ./main.py ./output/output.csv
