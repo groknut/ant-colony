5e7dbcc3e148d3b1431ddbc180ff297ef1c577c3
groknut <184639205+groknut@users.noreply.github.com>
2025-12-17 16:57:40 +0800

    update start_cpp.py

--- py/start_cpp.py
+++ py/start_cpp.py
@@ -1,5 +1,3 @@
-
-
 import subprocess as sp
 import os
 from pathlib import Path
@@ -7,23 +5,31 @@ import sys
 from config import timeit
 
 @timeit
-def compile_cpp():
+def compile_cpp(output_file):
 
-    exe_path = "main.exe"  if os.name == "nt" else "main"
+    exe_path = str(Path(output_file).with_suffix('.exe'))  if os.name == "nt" else str(Path(output_file))
 
     if not os.path.exists(exe_path):
-        sp.run(f"g++ -Ilibs/cfig  main.cpp src/*.cpp libs/cfig/cfig.cpp -o main", shell=True)
+        sp.run(f"g++ -Ilibs/cfig  main.cpp src/*.cpp libs/cfig/cfig.cpp -o {output_file}", shell=True)
 
 @timeit
-def start_cpp():
-    exe_path = "main.exe"  if os.name == "nt" else "main"
+def start_cpp(output_file):
+    exe_path = str(Path(output_file).with_suffix('.exe'))  if os.name == "nt" else str(Path(output_file))
     sp.run(
-        f"{exe_path}", shell=True
+        f"{output_file}", shell=True
     )
 
 def main():
-    compile_cpp()
-    start_cpp()
+    if len(sys.argv) != 2:
+        print(f"""
+Usage: python {sys.argv[0]} <output_file>
+Example: python {sys.argv[0]} main
+        """)
+        sys.exit(0)
+    else:
+        compile_cpp(sys.argv[1])
+        start_cpp(sys.argv[1])
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
